610d4aac4ff149a3129e8d73254091facb3dd0e8
Murod <makhmatkulov4828@gmail.com>
2024-09-17 18:43:16 +0500

    lessons added

---
+++ lesson group_by_1.sql
@@ -0,0 +1,139 @@
+select * from sales.orders;
+
+
+SELECT
+    customer_id,
+    YEAR (order_date) order_year
+FROM
+    sales.orders
+WHERE
+    customer_id IN (1, 2)
+ORDER BY
+    customer_id;
+
+
+select * from sales.orders;
+
+SELECT
+    customer_id,
+    YEAR (order_date) order_year
+FROM
+    sales.orders
+WHERE
+    customer_id IN (1, 2)
+GROUP BY
+    customer_id,
+    YEAR (order_date)
+ORDER BY
+    customer_id;
+
+
+
+SELECT DISTINCT
+    customer_id,
+    YEAR (order_date) order_year
+FROM
+    sales.orders
+WHERE
+    customer_id IN (1, 2)
+ORDER BY
+    customer_id;
+
+
+SELECT
+    customer_id,
+    YEAR (order_date) order_year,
+    COUNT (order_id) order_placed
+FROM
+    sales.orders
+WHERE
+    customer_id IN (1, 2)
+GROUP BY
+    customer_id,
+    YEAR (order_date)
+ORDER BY
+    customer_id; 
+
+
+
+SELECT
+    customer_id,
+    YEAR (order_date) order_year,
+    order_status
+FROM
+    sales.orders
+WHERE
+    customer_id IN (1, 2)
+GROUP BY
+    customer_id,
+    YEAR (order_date)
+ORDER BY
+    customer_id;
+
+
+
+SELECT
+    city,
+    COUNT (customer_id) customer_count
+FROM
+    sales.customers
+GROUP BY
+    city
+ORDER BY
+    city;
+
+
+
+SELECT
+    city,
+    state,
+    COUNT (customer_id) customer_count
+FROM
+    sales.customers
+GROUP BY
+    state,
+    city
+ORDER BY
+    city,
+    state;
+
+
+SELECT
+    brand_name,
+    MIN (list_price) min_price,
+    MAX (list_price) max_price
+FROM
+    production.products p
+INNER JOIN production.brands b ON b.brand_id = p.brand_id
+WHERE
+    model_year = 2018
+GROUP BY
+    brand_name
+ORDER BY
+    brand_name;
+
+
+SELECT
+    brand_name,
+    AVG (list_price) avg_price
+FROM
+    production.products p
+INNER JOIN production.brands b ON b.brand_id = p.brand_id
+WHERE
+    model_year = 2018
+GROUP BY
+    brand_name
+ORDER BY
+    brand_name;
+
+
+SELECT
+    order_id,
+    SUM (
+        quantity * list_price * (1 - discount)
+    ) net_value
+FROM
+    sales.order_items
+GROUP BY
+    order_id;
+
